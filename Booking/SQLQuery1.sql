e62fd8627b68aa5f10705c4bd1d9ea20c448b072
BibianaSebestianova <b.sebestianova@gmail.com>
2017-04-22 15:38:45 +0200

    Search to editable ComboBox

--- Booking/SQLQuery1.sql
+++ Booking/SQLQuery1.sql
@@ -1,55 +1,41 @@
 ﻿
-insert into d_destination 
-( d_city, d_country)
-values
-('Bratislava', 'Slovakia');
-insert into d_destination (d_city, d_country) values ('Nartkala', 'Russia');
-insert into d_destination (d_city, d_country) values ('Overyata', 'Russia');
-insert into d_destination (d_city, d_country) values ('Strelka', 'Russia');
-insert into d_destination (d_city, d_country) values ('Baltimore', 'United States');
-insert into d_destination (d_city, d_country) values ('Jeffersonville', 'United States');
-insert into d_destination (d_city, d_country) values ('Bukovany', 'Czech Republic');
-insert into d_destination (d_city, d_country) values ('Washington', 'United States');
-insert into d_destination (d_city, d_country) values ('Tucson', 'United States');
-insert into d_destination (d_city, d_country) values ('Stęszew', 'Poland');
-insert into d_destination (d_city, d_country) values ('Bakersfield', 'United States');
-insert into d_destination (d_city, d_country) values ('Višnjićevo', 'Serbia');
-insert into d_destination (d_city, d_country) values ('Kumberg', 'Austria');
-insert into d_destination (d_city, d_country) values ('Kirovsk', 'Russia');
-insert into d_destination (d_city, d_country) values ('La Roche-sur-Yon', 'France');
-insert into d_destination (d_city, d_country) values ('Krivyanskaya', 'Russia');
-insert into d_destination (d_city, d_country) values ('Biała Piska', 'Poland');
-insert into d_destination (d_city, d_country) values ('Żyraków', 'Poland');
-insert into d_destination (d_city, d_country) values ('Verkhniy Avzyan', 'Russia');
-insert into d_destination (d_city, d_country) values ('Vysotsk', 'Russia');
-insert into d_destination (d_city, d_country) values ('Mirsk', 'Poland');
-insert into d_destination (d_city, d_country) values ('Vojkovice', 'Czech Republic');
-insert into d_destination (d_city, d_country) values ('El Paso', 'United States');
-insert into d_destination (d_city, d_country) values ('Mollas', 'Albania');
-insert into d_destination (d_city, d_country) values ('Ishëm', 'Albania');
-insert into d_destination (d_city, d_country) values ('Rochester', 'United States');
-insert into d_destination (d_city, d_country) values ('Krasnaya Gorka', 'Russia');
-insert into d_destination (d_city, d_country) values ('Szklarska Poręba', 'Poland');
-insert into d_destination (d_city, d_country) values ('Uście Gorlickie', 'Poland');
-insert into d_destination (d_city, d_country) values ('Sremski Karlovci', 'Serbia');
-insert into d_destination (d_city, d_country) values ('Torino', 'Italy');
-insert into d_destination (d_city, d_country) values ('Créteil', 'France');
-insert into d_destination (d_city, d_country) values ('Łęki', 'Poland');
-insert into d_destination (d_city, d_country) values ('Rouen', 'France');
-insert into d_destination (d_city, d_country) values ('Uptar', 'Russia');
-insert into d_destination (d_city, d_country) values ('Bagan', 'Russia');
-insert into d_destination (d_city, d_country) values ('Krutinka', 'Russia');
-insert into d_destination (d_city, d_country) values ('Bronx', 'United States');
-insert into d_destination (d_city, d_country) values ('Przecław', 'Poland');
-insert into d_destination (d_city, d_country) values ('Sieradza', 'Poland');
-insert into d_destination (d_city, d_country) values ('Gornji Breg', 'Serbia');
-insert into d_destination (d_city, d_country) values ('Yam', 'Russia');
-insert into d_destination (d_city, d_country) values ('Marseille', 'France');
-insert into d_destination (d_city, d_country) values ('Glubokiy', 'Russia');
-insert into d_destination (d_city, d_country) values ('Fryčovice', 'Czech Republic');
-insert into d_destination (d_city, d_country) values ('Sandvika', 'Norway');
-insert into d_destination (d_city, d_country) values ('Lukićevo', 'Serbia');
-insert into d_destination (d_city, d_country) values ('Karaidel’', 'Russia');
-insert into d_destination (d_city, d_country) values ('Głogów Małopolski', 'Poland');
-insert into d_destination (d_city, d_country) values ('Vykhino-Zhulebino', 'Russia');
-insert into d_destination (d_city, d_country) values ('El Paso', 'United States');
+insert into d_destination ( d_city, d_country)values('Bratislava', 'Slovakia');
+insert into d_destination (d_city, d_country) values ('Moscow', 'Russia');
+insert into d_destination (d_city, d_country) values ('Warsaw', 'Poland');
+insert into d_destination (d_city, d_country) values ('Rome', 'Italy');
+insert into d_destination (d_city, d_country) values ('Paris', 'France');
+insert into d_destination (d_city, d_country) values ('Belgrade', 'Serbia');
+insert into d_destination (d_city, d_country) values ('Tirana', 'Albania');
+insert into d_destination (d_city, d_country) values ('Prague', 'Czech Republic');
+insert into d_destination (d_city, d_country) values ('Vienna', 'Austria');
+insert into d_destination (d_city, d_country) values ('Berlin', 'Germany');
+
+
+
+insert into h_hotel  ( h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ( 'Moonlight Hotel', 3 , 'Jana Street', 2572, 'Wien', 'Bypass Right External Iliac Artery to Bilateral Internal Iliac Arteries, Open Approach', 'Austria');
+
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Spectrum Hotel', 5, '729 Evergreen Point', 06865, 'Prague', 'Drainage of Right Upper Extremity Bursa and Ligament, Open Approach', 'Czech Republic');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Sanctum Hotel', 4, '63817 Summer Ridge Plaza', 9, 'Prague', 'Supplement Cervical Vertebral Joint with Nonautologous Tissue Substitute, Percutaneous Approach', 'Czech Republic');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Northern Nimbus Hotel', 5, '5961 Killdeer Parkway', 31, 'Bratislava', 'Revision of Synthetic Substitute in Left Lower Extremity, Open Approach', 'Slovakia');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Silver Shore Resort', 2, '911 Maple Wood Trail', 8, 'Belgrade', 'Replacement of Intracranial Artery with Synthetic Substitute, Open Approach', 'Serbia');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Moonlight Hotel', 3, '19323 Sherman Hill', 5654, 'Belgrade', 'Reposition Head and Neck Bursa and Ligament, Percutaneous Endoscopic Approach', 'Serbia');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Azure Basin Hotel', 4, '10 Oak Valley Crossing', 54, 'Prague', 'Gait Training/Functional Ambulation Treatment using Orthosis', 'Czech Republic');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Hilton', 2, '12770 Onsgard Park', 14392, 'Bratislava', 'Removal of Autologous Tissue Substitute from Right Extraocular Muscle, Open Approach', 'Slovakia');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Spectrum Hotel', 1, '3293 Ridgeview Road', 014, 'Tirana', 'Dilation of Right Eustachian Tube, Percutaneous Approach', 'Albania');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Silver Shore Resort', 2, '26151 Nobel Junction', 9, 'Warsaw', 'Dilation of Aortic Valve, Percutaneous Approach', 'Poland');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Deluxe Resort', 4, '09 Park Meadow Drive', 8394, 'Moscow', 'Revision of Synthetic Substitute in Upper Vein, Percutaneous Endoscopic Approach', 'Russia');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Azure Basin Hotel', 5, '7685 Fairview Court', 8, 'Moscow', 'Release Facial Muscle, External Approach', 'Russia');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Hilton', 3, '2632 Blaine Alley', 0861, 'Prague', 'Drainage of Abdominal Wall with Drainage Device, Percutaneous Endoscopic Approach', 'Czech Republic');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Hilton', 1, '92 Cardinal Center', 54728, 'Tirana', 'Division of Ulnar Nerve, Percutaneous Approach', 'Albania');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Spectrum Hotel', 2, '07 Manley Circle', 6145, 'Belgrade', 'Restriction of Accessory Pancreatic Duct, Open Approach', 'Serbia');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Crown Plaza', 5, '9458 Green Ridge Way', 4121, 'Bratislava', 'Excision of Nasopharynx, Via Natural or Artificial Opening', 'Slovakia');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Deluxe Resort', 1, '3 Fieldstone Terrace', 2209, 'Berlin', 'Occlusion of Right External Iliac Artery, Open Approach', 'Germany');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Silver Shore Resort', 2, '482 Old Shore Plaza', 1, 'Warsaw', 'Revision of Internal Fixation Device in Left Scapula, Open Approach', 'Poland');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Northern Nimbus Hotel', 4, '2 Gerald Trail', 95, 'Belgrade', 'Computerized Tomography (CT Scan) of Left Elbow using Other Contrast', 'Serbia');
+insert into dbo.h_hotel (h_name, h_stars, h_address, h_zip, h_d_city, h_description, h_d_country) values ('Olive Angel Resort', 5, '95259 Loftsgordon Crossing', 6, 'Rome', 'Restriction of Left External Carotid Artery with Bioactive Intraluminal Device, Percutaneous Approach', 'Italy');
+
+
+
+
+
+
